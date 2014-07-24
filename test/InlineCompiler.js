3eb715b191f4631135ffd0aaf525b8241aeafd59
ian.moore <ian@incamoon.com>
2014-07-24 12:30:09 -0700

    added plugin capability switched extends.js with inherits.js

--- test/InlineCompiler.js
+++ test/InlineCompiler.js
@@ -1,24 +1,24 @@
 "use strict";
 var assert = require('assert')
-  , InlineCompiler = require('../lib/inline');
+    , InlineCompiler = require('../lib/inline');
 
-describe('InlineCompiler', function() {
+describe('InlineCompiler', function () {
 
-  describe('with default configuration', function() {
+  describe('with default configuration', function () {
 
     var c = new InlineCompiler();
 
-    it('should emit generic chars as is', function() {
+    it('should emit generic chars as is', function () {
       assert.equal(c.toHtml("Hello world!"), "Hello world!");
     });
 
-    it('should emit backslashed special chars as is', function() {
+    it('should emit backslashed special chars as is', function () {
       assert.equal(c.toHtml("\\\\"), "\\");
       assert.equal(c.toHtml("Test\\."), "Test.");
       assert.equal(c.toHtml("\\Test\\."), "\\Test.");
     });
 
-    it('should escape &amp;, except in entity references', function() {
+    it('should escape &amp;, except in entity references', function () {
       assert.equal(c.toHtml("this & that"), "this &amp; that");
       assert.equal(c.toHtml("this &amp; that"), "this &amp; that");
       assert.equal(c.toHtml("this &#026; that"), "this &#026; that");
@@ -29,74 +29,74 @@ describe('InlineCompiler', function() {
       assert.equal(c.toHtml(input), output);
     });
 
-    it('should escape &lt;, except in HTML tags', function() {
+    it('should escape &lt;, except in HTML tags', function () {
       assert.equal(c.toHtml("a < b"), "a &lt; b");
       assert.equal(c.toHtml("a <b>c</b>"), "a <b>c</b>");
     });
 
-    it('should leave HTML comments unprocessed', function() {
+    it('should leave HTML comments unprocessed', function () {
       var input = "This & <!-- this text is *unprocessed* --> that.";
       var output = "This &amp; <!-- this text is *unprocessed* --> that.";
       assert.equal(c.toHtml(input), output);
     });
 
-    it('should escape &gt; chars', function() {
+    it('should escape &gt; chars', function () {
       var input = "A < B; B > C";
       var output = "A &lt; B; B &gt; C";
       assert.equal(c.toHtml(input), output);
     });
 
-    it('should process HTML tags coarsely, without real HTML semantics', function() {
+    it('should process HTML tags coarsely, without real HTML semantics', function () {
       var input = "A<B *hello* A>B";
       var output = "A<B *hello* A>B";
       assert.equal(c.toHtml(input), output);
     });
 
-    it('should process ems and strongs', function() {
+    it('should process ems and strongs', function () {
       assert.equal(c.toHtml("Text *_ * _*"), "Text <strong>_ </strong> _*");
       assert.equal(c.toHtml("Text _ * _*"), "Text <em> * </em>*");
     });
 
-    it('should respect backslashes while on ems/strongs', function() {
+    it('should respect backslashes while on ems/strongs', function () {
       assert.equal(
-        c.toHtml("Text *_ \\* _*"),
-        "Text <strong><em> * </em></strong>");
+          c.toHtml("Text *_ \\* _*"),
+          "Text <strong><em> * </em></strong>");
     });
 
-    it('should process triple code spans', function() {
+    it('should process triple code spans', function () {
       assert.equal(
-        c.toHtml("Code with ```<b>tags</b> & SGMLs &#095;```"),
-        "Code with <code><b>tags</b> & SGMLs &#095;</code>");
+          c.toHtml("Code with ```<b>tags</b> & SGMLs &#095;```"),
+          "Code with <code><b>tags</b> & SGMLs &#095;</code>");
     });
 
-    it('should process regular code spans', function() {
+    it('should process regular code spans', function () {
       assert.equal(
-        c.toHtml("Code with `<b>tags</b> & SGMLs &#095;`"),
-        "Code with <code>&lt;b&gt;tags&lt;/b&gt; &amp; SGMLs &#095;</code>");
+          c.toHtml("Code with `<b>tags</b> & SGMLs &#095;`"),
+          "Code with <code>&lt;b&gt;tags&lt;/b&gt; &amp; SGMLs &#095;</code>");
     });
 
-    it('should process MathJax formulas', function() {
+    it('should process MathJax formulas', function () {
       assert.equal(
-        c.toHtml("%%e^i\\varphi = \\cos{\\varphi} + i\\sin{\\varphi}%%"),
-        "%%e^i\\varphi = \\cos{\\varphi} + i\\sin{\\varphi}%%");
+          c.toHtml("%%e^i\\varphi = \\cos{\\varphi} + i\\sin{\\varphi}%%"),
+          "%%e^i\\varphi = \\cos{\\varphi} + i\\sin{\\varphi}%%");
       assert.equal(c.toHtml("$$a<b>c<d>f$$"), "$$a&lt;b&gt;c&lt;d&gt;f$$");
     });
 
-    it('should leave link definitions and media without processing', function() {
+    it('should leave link definitions and media without processing', function () {
       assert.equal(
-        c.toHtml("Headless [[link]]"),
-        "Headless [[link]]");
+          c.toHtml("Headless [[link]]"),
+          "Headless [[link]]");
       assert.equal(
-        c.toHtml("Referenced [link][id]"),
-        "Referenced [link][id]");
+          c.toHtml("Referenced [link][id]"),
+          "Referenced [link][id]");
       assert.equal(
-        c.toHtml("Referenced ![media][id]"),
-        "Referenced ![media][id]");
+          c.toHtml("Referenced ![media][id]"),
+          "Referenced ![media][id]");
     });
 
   });
 
-  describe("with some test links and media", function() {
+  describe("with some test links and media", function () {
 
     var links = {
       rho: {
@@ -114,67 +114,108 @@ describe('InlineCompiler', function() {
     };
 
     var c = new InlineCompiler({
-      resolveLink: function(id) {
+      resolveLink: function (id) {
         return links[id];
       },
-      resolveImage: function(id) {
+      resolveImage: function (id) {
         return images[id];
       }
     });
 
-    it("should resolve headless links", function() {
+    it("should resolve headless links", function () {
       assert.equal(
-        c.toHtml("Everybody likes [[rho]]"),
-        "Everybody likes <a href=\"http://github.com/inca/rho\"" +
-          " title=\"Rho — text2html processing tool for Node\">" +
-          "Rho — text2html processing tool for Node</a>");
+          c.toHtml("Everybody likes [[rho]]"),
+              "Everybody likes <a href=\"http://github.com/inca/rho\"" +
+              " title=\"Rho — text2html processing tool for Node\">" +
+              "Rho — text2html processing tool for Node</a>");
     });
 
-    it("should resolve reference links", function() {
+    it("should resolve reference links", function () {
       assert.equal(
-        c.toHtml("[Rho][rho] — text2html processing tool for [Node][node]."),
-        "<a href=\"http://github.com/inca/rho\"" +
-          " title=\"Rho — text2html processing tool for Node\">Rho</a> — " +
-          "text2html processing tool for <a href=\"http://nodejs.org\">Node</a>.");
+          c.toHtml("[Rho][rho] — text2html processing tool for [Node][node]."),
+              "<a href=\"http://github.com/inca/rho\"" +
+              " title=\"Rho — text2html processing tool for Node\">Rho</a> — " +
+              "text2html processing tool for <a href=\"http://nodejs.org\">Node</a>.");
     });
 
-    it("should process inline links", function() {
+    it("should process inline links", function () {
       assert.equal(
-        c.toHtml("[Rho](https://github.com/inca/rho) is awesome!"),
-        "<a href=\"https://github.com/inca/rho\">Rho</a> is awesome!");
+          c.toHtml("[Rho](https://github.com/inca/rho) is awesome!"),
+          "<a href=\"https://github.com/inca/rho\">Rho</a> is awesome!");
     });
 
-    it("should resolve reference images", function() {
+    it("should resolve reference images", function () {
       assert.equal(
-        c.toHtml("Me: ![Boris Okunskiy][gravatar]"),
-        "Me: <img src=\"http://gravatar.com/avatar/e1e3018a2ed287d8bae27bacdabefcb6\"" +
-          " alt=\"Boris Okunskiy\" title=\"Look at me!\"/>");
+          c.toHtml("Me: ![Boris Okunskiy][gravatar]"),
+              "Me: <img src=\"http://gravatar.com/avatar/e1e3018a2ed287d8bae27bacdabefcb6\"" +
+              " alt=\"Boris Okunskiy\" title=\"Look at me!\"/>");
     });
 
-    it("should process inline images", function() {
+    it("should process inline images", function () {
       assert.equal(
-        c.toHtml("Me: ![](http://gravatar.com/avatar/e1e3018a2ed287d8bae27bacdabefcb6)"),
-        "Me: <img src=\"http://gravatar.com/avatar/e1e3018a2ed287d8bae27bacdabefcb6\"" +
-          " alt=\"\"/>");
+          c.toHtml("Me: ![](http://gravatar.com/avatar/e1e3018a2ed287d8bae27bacdabefcb6)"),
+              "Me: <img src=\"http://gravatar.com/avatar/e1e3018a2ed287d8bae27bacdabefcb6\"" +
+              " alt=\"\"/>");
     });
 
-    it("should process cool things inside inline link texts", function() {
+    it("should process cool things inside inline link texts", function () {
       assert.equal(
-        c.toHtml("[The `code` inside a link](#) is cool."),
+          c.toHtml("[The `code` inside a link](#) is cool."),
           "<a href=\"#\">The <code>code</code> inside a link</a> is cool.");
     });
 
-    it("should process cool things inside reference link texts", function() {
+    it("should process cool things inside reference link texts", function () {
       assert.equal(
-        c.toHtml("[The `code` inside a link][node] is cool."),
-        "<a href=\"http://nodejs.org\">The <code>code</code> inside a link</a>" +
-          " is cool.");
+          c.toHtml("[The `code` inside a link][node] is cool."),
+              "<a href=\"http://nodejs.org\">The <code>code</code> inside a link</a>" +
+              " is cool.");
     });
 
-    it("should resolve links inside HTML attributes", function() {
+    it("should resolve links inside HTML attributes", function () {
       assert.equal(
-        c.toHtml("<a href=\"[node]\">NodeJS</a>."),
-        "<a href=\"http://nodejs.org\">NodeJS</a>.");
+          c.toHtml("<a href=\"[node]\">NodeJS</a>."),
+          "<a href=\"http://nodejs.org\">NodeJS</a>.");
+    });
+
+  });
+
+  describe("with some plugins", function () {
+
+    var c = new InlineCompiler({
+      plugins: [
+        function (walk, out) {
+          if (!walk.at("~Green[")) return false;
+          // Try to find the ]
+          walk.skip(7);
+          var start = walk.position;
+          var end = walk.indexOf("]");
+          if (end === null) {
+            this.out.push("~Green[");
+            return true;
+          }
+          // Collecting the text up to ] and matching further
+          var text = walk.yieldUntil(end);
+          walk.skip();
+          out.push('<span class="co co-green">' + text + '</span>');
+          return true;
+        }
+      ]
+    });
+
+    var d = new InlineCompiler();
+
+    it("should work with a simple plugin", function () {
+      assert.equal(c.toHtml("~Green[foo] da bar bar"), '<span class="co co-green">foo</span> da bar bar');
+    });
+
+    it('should emit generic chars as is', function () {
+      assert.equal(c.toHtml("Hello world!"), "Hello world!");
+    });
+
+    it("should work without a plugin", function () {
+      assert.equal(
+          d.toHtml("~[foo]~"),
+          "~[foo]~");
     });
 
   });
